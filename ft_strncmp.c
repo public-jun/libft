4ee340623723043061e867739a30e41eb12fd4b8
public-jun <generalpublicjunjun@gmail.com>
2021-05-03 13:53:29 +0900

    null gurd

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jnakahod <jnakahod@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/12 19:45:29 by jnakahod          #+#    #+#             */
-/*   Updated: 2021/04/05 19:18:45 by jnakahod         ###   ########.fr       */
+/*   Updated: 2021/05/03 13:48:11 by jnakahod         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	unsigned char	*str1;
 	unsigned char	*str2;
 
+	if (!s1 || !s2)
+		return (-1);
 	str1 = (unsigned char *)s1;
 	str2 = (unsigned char *)s2;
 	while (n-- > 0)
